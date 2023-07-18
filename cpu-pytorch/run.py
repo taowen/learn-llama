8af5debfed74ad15100eeec57eb5dd0b97c1d486
Tao Wen <taowen@gmail.com>
2023-07-18 21:13:53 +0800

    update

--- cpu-pytorch/run.py
+++ cpu-pytorch/run.py
@@ -1,21 +1,68 @@
 from dataclasses import dataclass
 
+@dataclass
+class GlobalCache:
+    device: any
+    embed_tokens = None
+    config: any = None
+    head_dim: any = None
+    inv_freq: any = None
+    rotary_emb: any = None
+
+@dataclass
+class LayerCache:
+    index: int
+    input_layernorm_weight: any = None
+    post_attention_layernorm_weight: any = None
+    q_proj: any = None
+    k_proj: any = None
+    v_proj: any = None
+    o_proj: any = None
+
 def main():
     import torch
     with torch.inference_mode():
         cache = GlobalCache(device=torch.device('cpu'))
         layer1 = LayerCache(index=0)
-        decode_layer(cache, layer1, input_embeds=input_embeds(cache))
+        input_ids = torch.tensor(tokenize(cache, 'Once upon a time, '), dtype=torch.long).unsqueeze(0)
+        input_embeds = embed_tokens(cache).forward(input_ids)
+        decode_layer(cache, layer1, input_embeds=input_embeds)
 
-@dataclass
-class GlobalCache:
-    device: any
-    config: any = None
-    input_ids: any = None # REMOVE THIS
-    input_embeds: any = None # REMOVE THIS
-    head_dim: any = None
-    inv_freq: any = None
-    rotary_emb: any = None
+def decode_layer(cache: GlobalCache, layer: LayerCache, input_embeds):
+    input_layernormed = input_layernorm(cache, layer, input_embeds)
+    attn_output = self_attn(cache, layer, input_layernormed)
+    # input_embeds is residual
+    attn_output = input_embeds + attn_output
+    attn_output_layernormed = post_attention_layernorm(cache, layer, attn_output)
+    print(attn_output_layernormed[0][0])
+    return attn_output
+
+def input_layernorm(cache: GlobalCache, layer: LayerCache, input_embeds):
+    return rms_layernorm(cache, input_layernorm_weight(layer), input_embeds)
+
+def post_attention_layernorm(cache: GlobalCache, layer: LayerCache, input_embeds):
+    return rms_layernorm(cache, post_attention_layernorm_weight(layer), input_embeds)
+
+def rms_layernorm(cache: GlobalCache, weight, input_embeds):
+    import torch
+    variance = input_embeds.to(torch.float32).pow(2).mean(-1, keepdim=True)
+    input_layernormed = input_embeds * torch.rsqrt(variance + model_config(cache)['rms_norm_eps'])
+    input_layernormed = (weight * input_layernormed).to(input_embeds.dtype)
+    return input_layernormed
+
+def input_layernorm_weight(layer: LayerCache):
+    from torch import nn
+    if layer.input_layernorm_weight is None:
+        weight = safetensors_00001(f'model.layers.{layer.index}.input_layernorm.weight')
+        layer.input_layernorm_weight = nn.Parameter(weight)
+    return layer.input_layernorm_weight
+
+def post_attention_layernorm_weight(layer: LayerCache):
+    from torch import nn
+    if layer.post_attention_layernorm_weight is None:
+        weight = safetensors_00001(f'model.layers.{layer.index}.post_attention_layernorm.weight')
+        layer.post_attention_layernorm_weight = nn.Parameter(weight)
+    return layer.post_attention_layernorm_weight
 
 def model_path():
     return '../weights/open_llama_3b_v2_safetensors'
@@ -31,27 +78,12 @@ def model_config(cache: GlobalCache):
     # {'architectures': ['LlamaForCausalLM'], 'bos_token_id': 1, 'eos_token_id': 2, 'hidden_act': 'silu', 'hidden_size': 3200, 'initializer_range': 0.02, 'intermediate_size': 8640, 'max_position_embeddings': 2048, 'model_type': 'llama', 'num_attention_heads': 32, 'num_hidden_layers': 26, 'pad_token_id': 0, 'rms_norm_eps': 1e-06, 'tie_word_embeddings': False, 'torch_dtype': 'float16', 'transformers_version': '4.31.0.dev0', 'use_cache': True, 'vocab_size': 32000}
     return cache.config
 
-def sp_input_ids(cache: GlobalCache):
+def tokenize(cache: GlobalCache, input: str):
     import sentencepiece
     sp = sentencepiece.SentencePieceProcessor()
     sp.load(f'{model_path()}/tokenizer.model')
     # [0, 9038, 2501, 263, 931, 29892, 29871]
-    return [model_config(cache)['bos_token_id']] + sp.encode('Once upon a time, ')
-
-def input_ids(cache: GlobalCache):
-    # convert input_ids from long[] to 2D tensor (batch_size, sequence_length)
-    # we only have 1 batch here, so unsqueeze(0)
-    # The 0 in torch.unsqueeze(input, 0) indicates the dimension index at which you want to insert the new axis.
-    # By convention:
-    # Dimension 0 is the batch dimension
-    # Dimension 1 is the sequence length dimension
-    # Dimension 2 is the embedding dimension
-    # This inserts a new axis (dimension) of size 1 at index 0, which becomes the new batch dimension.
-    if cache.input_ids:
-        return cache.input_ids
-    import torch
-    cache.input_ids = torch.tensor(sp_input_ids(cache), dtype=torch.long).unsqueeze(0)
-    return cache.input_ids
+    return [model_config(cache)['bos_token_id']] + sp.encode(input)
 
 def safetensors_00001(key):
     from safetensors import safe_open
@@ -60,63 +92,43 @@ def safetensors_00001(key):
     
 def embed_tokens(cache: GlobalCache):
     import torch
-    # the embedding layer created from embed_tokens_tensor and config.json
-    return torch.nn.Embedding(
-        model_config(cache)['vocab_size'], model_config(cache)['hidden_size'], model_config(cache)['pad_token_id'], 
-        _weight=safetensors_00001('model.embed_tokens.weight'))
-
-def input_embeds(cache: GlobalCache):
-    if cache.input_embeds is not None:
-        return cache.input_embeds
-    cache.input_embeds = embed_tokens(cache).forward(input_ids(cache))
-    return cache.input_embeds
-
-# def causal_mask():
-#     global cache_causal_mask
-#     if 'cache_causal_mask' in globals():
-#         return cache_causal_mask
-#     import torch
-#     bsz, seq_len = input_ids().size()
-#     device = input_embeds().device
-#     causal_mask = torch.full((seq_len, seq_len), torch.tensor(torch.finfo(input_embeds().dtype).min, device=device), device=device)
-#     mask_cond = torch.arange(causal_mask.size(-1), device=device)
-#     causal_mask.masked_fill_(mask_cond < (mask_cond + 1).view(causal_mask.size(-1), 1), 0)
-#     causal_mask = causal_mask.to(input_embeds().dtype)
-#     causal_mask = causal_mask[None, None, :, :].expand(bsz, 1, seq_len, seq_len)
-#     return causal_mask
-
-# def position_ids():
-#     global cache_position_ids
-#     if 'cache_position_ids' in globals():
-#         return cache_position_ids
-#     import torch
-#     bsz, seq_len = input_ids().size()
-#     cache_position_ids = torch.arange(
-#         0, seq_len, dtype=torch.long, device=input_embeds().device
-#     )
-#     cache_position_ids = cache_position_ids.unsqueeze(0).view(-1, seq_len)
-#     return cache_position_ids
-
-@dataclass
-class LayerCache:
-    index: int
-    input_layernorm: any = None
-    q_proj: any = None
-    k_proj: any = None
-    v_proj: any = None
-
-def input_layernorm(layer: LayerCache):
-    from torch import nn
-    if layer.input_layernorm is None:
-        weight = safetensors_00001(f'model.layers.{layer.index}.input_layernorm.weight')
-        layer.input_layernorm = nn.Parameter(weight)
-    return layer.input_layernorm
+    if cache.embed_tokens is None:
+        config = model_config(cache)
+        cache.embed_tokens = torch.nn.Embedding(config['vocab_size'], config['hidden_size'], config['pad_token_id'])
+        cache.embed_tokens.weight = torch.nn.Parameter(safetensors_00001('model.embed_tokens.weight'))
+    return cache.embed_tokens
 
 def head_dim(cache: GlobalCache):
     if cache.head_dim is None:
         config = model_config(cache)
         cache.head_dim = config['hidden_size'] // config['num_attention_heads']
     return cache.head_dim
+
+def self_attn(cache: GlobalCache, layer: LayerCache, input_layernormed):
+    import torch
+    from torch import nn
+    import math
+    bsz, q_len, _ = input_layernormed.size()
+    config = model_config(cache)
+    query_states = q_proj(cache, layer).forward(input_layernormed).view(bsz, q_len, config['num_attention_heads'], head_dim(cache)).transpose(1, 2)
+    key_states = k_proj(cache, layer).forward(input_layernormed).view(bsz, q_len, config['num_attention_heads'], head_dim(cache)).transpose(1, 2)
+    value_states = v_proj(cache, layer).forward(input_layernormed).view(bsz, q_len, config['num_attention_heads'], head_dim(cache)).transpose(1, 2)
+    kv_seq_len = key_states.shape[-2]
+    cos, sin = rotary_emb(cache, kv_seq_len, value_states.dtype)
+    position_ids = position_ids_of_seq(cache, kv_seq_len)
+    query_states, key_states = apply_rotary_pos_emb(query_states, key_states, cos, sin, position_ids)
+    attn_weights = torch.matmul(query_states, key_states.transpose(2, 3)) / math.sqrt(head_dim(cache))
+    causal_mask = causal_mask_of_seq(cache, kv_seq_len)
+    attn_weights = attn_weights + causal_mask
+    attn_weights = torch.max(
+        attn_weights, torch.tensor(torch.finfo(attn_weights.dtype).min, device=attn_weights.device)
+    )
+    attn_weights = nn.functional.softmax(attn_weights, dim=-1, dtype=torch.float32).to(query_states.dtype)
+    attn_output = torch.matmul(attn_weights, value_states)
+    attn_output = attn_output.transpose(1, 2)
+    attn_output = attn_output.reshape(bsz, q_len, config['hidden_size'])
+    attn_output = o_proj(cache, layer).forward(attn_output)
+    return attn_output
 
 def q_proj(cache: GlobalCache, layer: LayerCache):
     from torch import nn
@@ -141,6 +153,14 @@ def v_proj(cache: GlobalCache, layer: LayerCache):
         layer.v_proj = nn.Linear(config['hidden_size'], config['num_attention_heads'] * head_dim(cache), bias=False)
         layer.v_proj.weight = nn.Parameter(safetensors_00001(f'model.layers.{layer.index}.self_attn.v_proj.weight'))
     return layer.v_proj
+
+def o_proj(cache: GlobalCache, layer: LayerCache):
+    from torch import nn
+    if layer.o_proj is None:
+        config = model_config(cache)
+        layer.o_proj = nn.Linear(config['num_attention_heads'] * head_dim(cache), config['hidden_size'], bias=False)
+        layer.o_proj.weight = nn.Parameter(safetensors_00001(f'model.layers.{layer.index}.self_attn.o_proj.weight'))
+    return layer.o_proj
 
 def inv_freq(cache: GlobalCache):
     import torch
@@ -184,19 +204,19 @@ def apply_rotary_pos_emb(q, k, cos, sin, position_ids):
     k_embed = (k * cos) + (rotate_half(k) * sin)
     return q_embed, k_embed
 
-def decode_layer(cache: GlobalCache, layer: LayerCache, input_embeds):
+def position_ids_of_seq(cache:GlobalCache, seq_len):
     import torch
-    variance = input_embeds.to(torch.float32).pow(2).mean(-1, keepdim=True)
-    hidden_states = input_embeds * torch.rsqrt(variance + model_config(cache)['rms_norm_eps'])
-    hidden_states = (input_layernorm(layer) * hidden_states).to(input_embeds.dtype)
-    bsz, q_len, _ = hidden_states.size()
-    config = model_config(cache)
-    query_states = q_proj(cache, layer).forward(hidden_states).view(bsz, q_len, config['num_attention_heads'], head_dim(cache)).transpose(1, 2)
-    key_states = k_proj(cache, layer).forward(hidden_states).view(bsz, q_len, config['num_attention_heads'], head_dim(cache)).transpose(1, 2)
-    value_states = v_proj(cache, layer).forward(hidden_states).view(bsz, q_len, config['num_attention_heads'], head_dim(cache)).transpose(1, 2)
-    kv_seq_len = key_states.shape[-2]
-    cos, sin = rotary_emb(cache, kv_seq_len, value_states.dtype)
-    # cos, sin = self.rotary_emb(value_states, seq_len=kv_seq_len)
-    return hidden_states
+    return torch.arange(0, seq_len, dtype=torch.long, device=cache.device).unsqueeze(0).view(-1, seq_len)
+
+def causal_mask_of_seq(cache: GlobalCache, seq_len: int):
+    import torch
+    bsz = 1
+    device = cache.device
+    causal_mask = torch.full((seq_len, seq_len), torch.tensor(torch.finfo(torch.float32).min, device=device), device=device)
+    mask_cond = torch.arange(causal_mask.size(-1), device=device)
+    causal_mask.masked_fill_(mask_cond < (mask_cond + 1).view(causal_mask.size(-1), 1), 0)
+    causal_mask = causal_mask.to(torch.float32)
+    causal_mask = causal_mask[None, None, :, :].expand(bsz, 1, seq_len, seq_len)
+    return causal_mask
 
 main()
